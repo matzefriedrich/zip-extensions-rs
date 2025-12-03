1215f6acca25304695279a04a99248b02fccc569
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2025-12-03 23:21:14 +0100

    Introduces `zip-audit` example app

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.14.0] - Unreleased
+
+### Added
+
+* Add a new audit module for ZIP archives to identify security risks [#32](https://github.com/matzefriedrich/zip-extensions-rs/pull/32)
+
+
 ## [0.13.1] - 2025-12-20
 
 ### Fixed
---
+++ src/bin/audit/main.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::path::Path;
+use std::process;
+use zip_extensions::audit::zip_audit_reader::zip_audit;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <path_to_zipfile>", args[0]);
+        process::exit(1);
+    }
+
+    let file_path = &args[1];
+    let path = Path::new(file_path);
+
+    if !path.exists() {
+        eprintln!("Error: File '{}' does not exist.", file_path);
+        process::exit(1);
+    }
+
+    match zip_audit(path) {
+        Ok(report) => {
+            serde_json::to_writer_pretty(std::io::stdout(), &report).unwrap();
+            // println!("{:#?}", report);
+        }
+        Err(e) => {
+            eprintln!("Error auditing zip file: {}", e);
+            process::exit(1);
+        }
+    }
+}
